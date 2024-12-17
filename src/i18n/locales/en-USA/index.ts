3e1ec39a3a35810af38402386f57afa5ac7ca7f9
fass28 <fass28@icloud.com>
2024-12-17 18:12:37 -0500

    feat: spanish into personal-info section

--- src/i18n/locales/en-USA/index.ts
+++ src/i18n/locales/en-USA/index.ts
@@ -1,4 +1,5 @@
 import header from './header.json'
 import navbar from './navbar.json'
+import personal_info from './personal_info.json'
 
-export default { header, navbar }
+export default { header, navbar, personal_info }
